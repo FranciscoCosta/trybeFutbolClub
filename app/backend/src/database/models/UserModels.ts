71e670e1c102b75ba03fb441281ce0e38b9cf6a3
Xico <francisco100eg@gmail.com>
2023-02-27 19:51:14 -0300

    Req 6

---
+++ app/backend/src/database/models/UserModels.ts
@@ -0,0 +1,39 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  userName: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Team;
